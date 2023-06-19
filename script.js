853e43c225d87069cc14b2cb61264e8615a69ad6
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2023-06-19 18:59:45 -0500

    Takes both inputs and returns either if player wins or loses

--- script.js
+++ script.js
@@ -6,8 +6,38 @@ function getComputerChoice() {
   } else if (randomNum === 2) {
     return "paper";
   } else {
-    return "scissor";
+    return "scissors";
   }
 }
 
-console.log(getComputerChoice());
+const computerSelection = getComputerChoice();
+const playerSelection = prompt(
+  "Please select rock, paper or scissors"
+).toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection, computerSelection);
+  if (playerSelection === computerSelection) {
+    return "Its a tie";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      return "You Win! Rock beats Scissors";
+    } else {
+      return "You Lose! Paper beats Rock";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "You Win! Paper beats Rock";
+    } else {
+      return "You Lose! Scissors beats Paper";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      return "You Win! Scissors beat paper";
+    } else {
+      return "You Lose, Rock beats Scissors";
+    }
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
