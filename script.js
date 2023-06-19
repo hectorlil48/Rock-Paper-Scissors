1b2399df6ee92408d96bd860019f7ca4115dd9ef
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2023-06-19 17:19:11 -0500

    Created a function to get a random number to help pick a choice for the computer

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNum === 1) {
+    return "rock";
+  } else if (randomNum === 2) {
+    return "paper";
+  } else {
+    return "scissor";
+  }
+}
+
+console.log(getComputerChoice());
