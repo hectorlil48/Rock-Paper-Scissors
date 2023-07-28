66bcc990de77fea29a8634024593ede7a8430a25
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2023-07-28 18:03:15 -0500

    got selectors for my classes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Get selectors
 
+const buttons = document.querySelectorAll("button");
+
+const resultEl = document.querySelector(".result");
+
+const playerScore = document.querySelector(".user-score");
+
+const computerScore = document.querySelector(".computer-score");
